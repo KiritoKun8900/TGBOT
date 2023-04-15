640ecc6d2e5a2badabf7ec93b274099325f57e2f
KiritoKun8900 <130860406+KiritoKun8900@users.noreply.github.com>
2023-04-15 13:41:29 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv('API_ID','TU_API_ID'))
-    API_HASH = getenv('API_HASH','TU_HASH')
-    BOT_TOKEN = getenv('BOT_TOKEN','TU_BOT_TOKEN')
+    API_ID = int(getenv('API_ID','18722297'))
+    API_HASH = getenv('API_HASH','32c669fd369c589c3b62eb16531c42e6')
+    BOT_TOKEN = getenv('BOT_TOKEN','5599241378:AAHudNN_qNUiqjy4W1mJg6Ei99JW8Z2IjAM')
 
 config = Config()
